34aa5f44e342a3a1090c9e77906a3489bc663597
Sean Lee <seanboramlee@gmail.com>
2016-07-18 14:50:14 -0700

    finish all methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,9 +11,18 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    soda = @sodas.find { |soda| soda.brand == soda_brand}
+    if @sodas.include?(soda)
+      @cash += soda.price
+      @sodas.delete(soda)
+    else
+      nil
+    end
+    # @sodas.delete.soda
   end
 
 end
