5f703bdc92036ccef6e12fcc90620eb38b7ccf33
Youna Yang <email": null>
2016-07-18 16:06:17 -0700

    complete post checkpoint

--- soda_machine.rb
+++ soda_machine.rb
@@ -2,34 +2,32 @@ require_relative 'soda.rb'
 class SodaMachine
   attr_reader :sodas, :cash, :soda_machine
 
-
   def initialize(args = {})
     @sodas = args[:sodas]
-    @cash = args[:cash]
-    @soda_machine = { sodas: ["pepsi", "mountain_dew", "coke_zero", "second_pepsi"], cash: 1.00}
-    @pepsi = {brand: 'Pepsi', price: 0.65}
-    @mountain_dew = { brand: 'Mountain Dew', price: 0.75 }
-    @coke_zero = { brand: 'Coke Zero', price: 1.00 }
-    @second_pepsi = {brand: 'Pepsi', price: 0.65}
+    @cash = args[:cash] # total amt of money in machine
   end
 
   def current_inventory_count
-    soda_machine[:sodas].count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
-    @sodas.each do |x|
-    if x.brand == soda_brand
-       return x
-    end
-  end
-    return nil
-  end
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+       return soda
+      end
+   end
+   return nil
+ end
 
   def sell(soda_brand)
-    sodas.each { |k,v| p v }
-    soda_brand.price
-  end
-
+    @sodas.each_with_index do |soda, i|
+      if soda.brand == soda_brand
+       @cash =  @cash + soda.price     # add price to cash
+       @sodas.delete_at(i)   # remove coke zero from array
+      end
+   end
+   return nil
+ end
 
 end
