70bf4184ce7358f764ccae34ebff3e55e0721ffe
David <email": null>
2016-07-18 15:15:49 -0700

    Adds methods on lines 11 and 15 to pass rspec

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.count
   end
 
   def find_soda(soda_brand)
+   sodas.find {|soda| soda.brand == soda_brand ? soda :nil}
   end
 
   def sell(soda_brand)
+      if sodas.include?(soda_brand)
+        cash << soda.price
+      end
   end
-
 end
+
+
