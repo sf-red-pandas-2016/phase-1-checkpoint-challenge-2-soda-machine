956d28b5583be94513fa3640c12fec48e299c5e9
Kevin McGovern & Oshoke Maiki <pair+kevinmcgovern92.omaiki@devbootcamp.com>
2016-07-18 14:20:59 -0700

    Add current_inventory_count method, working

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,12 +6,17 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+#return the current inventory count
   def current_inventory_count
+    @sodas.count
   end
 
+#find a single soda with a given brand
   def find_soda(soda_brand)
   end
 
+#sell a soda with a given brand, and remove soda from the soda machine
+#should also add price of soda to soda machine's cash amount
   def sell(soda_brand)
   end
 
