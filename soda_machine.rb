f19c0bbbae3a7a97689cbaf247749323c4f236c6
Hayden Langelier <haydenlangelier@utexas.edu>
2016-07-18 15:28:32 -0700

    done

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|x|x.brand==soda_brand}
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      a=find_soda(soda_brand)
+      @cash +=a.price
+      @sodas.delete_if{|x|x.brand==soda_brand}
+    else
+      return nil
+    end
   end
 
 end
