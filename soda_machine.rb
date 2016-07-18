4a3e8468e180fafb8809bcf57af6ab1f3981444c
Parth Ahir <email": null>
2016-07-18 14:42:41 -0700

    Initial commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,13 +6,27 @@ class SodaMachine
     @cash = args[:cash]
   end
 
-  def current_inventory_count
+  def self.current_inventory_count
+    return @sodas.count
+
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      it soda.brand == soda.bring
+        return soda
+      else
+        return nil
+      end
+    end
+
   end
 
   def sell(soda_brand)
+    #
   end
 
 end
+
+
+d.find_soda(pepsi)
