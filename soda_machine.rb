273abc7caafd50b4cb315fabe5fa25132172391d
Nicholas Davidson <ndavidson.sf@me.com>
2016-07-18 17:10:49 -0700

    Adds breakout session solutions and notes as comments

--- soda_machine.rb
+++ soda_machine.rb
@@ -3,15 +3,27 @@ class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
-    @sodas = args[:sodas]
+    @sodas = args[:sodas] #=> Soda[]
     @cash = args[:cash]
   end
 
   def current_inventory_count
-    @sodas.count
+    @sodas.length #Even without the READER it will use the instance variable
+    # sodas.length #Using a method to access the variable, if you don't READ it, it can't find it.
+
+    # @sodas.count
+    # sodas.count
+
+    # self.sodas.count
+    # self.sodas.length
   end
 
   def find_soda(soda_brand)
+
+    # From breakout session
+    # @soda.select { |soda| soda.brand == soda_brand } # selects iterates over the whole array and returns all matches
+
+    # @soda.find { |soda| soda.brand == soda_brand } # this works because it returns nil; it is looking for the first one, if at all, if it finds one, it returns in.
 
     @sodas.each do |soda|
       if soda.brand == soda_brand
@@ -24,6 +36,15 @@ class SodaMachine
 
 
   def sell(soda_brand)
+
+    # found_soda = find_soda(soda_brand) # find_soda(soda_brand) is going to return nil or an object
+    # if found_soda
+    #   @cash += found_soda.price
+    #   @sodas.delete(found_soda)
+    # end
+    # found_soda
+
+
     @sodas.each do |soda|
       if soda.brand == soda_brand
 
